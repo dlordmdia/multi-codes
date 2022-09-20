2e4116e12000425e37850787361156f226852579
dlordmdia <shnuffi2.02@gmail.com>
2022-09-20 09:32:04 +0200

    Added Dlord en ruedas at dlord media launcher

--- multi-launcher.py
+++ multi-launcher.py
@@ -443,7 +443,7 @@ while True:
             print("\n--DLORD LAUNCHER--\n\n")
             time.sleep(1)
 
-            what1 = input("Qué quieres hacer?\n\na)Ver Contenido de Música\nb)Ver Contenido de Aventuras y Exteriores\nc)Ver Contenido de Drones\n\n-x to Leave\n-")
+            what1 = input("Qué quieres hacer?\n\na)Ver Contenido de Música\nb)Ver Contenido de Aventuras y Exteriores\nc)Ver Contenido de Drones\nd)Ver contenido De Bicicleta de Montaña\n\n-x to Leave\n-")
 
 
             if what1 =="a":
@@ -469,6 +469,14 @@ while True:
                 time.sleep(1)
                 os.system('cls' if os.name=='nt' else 'clear')
                 webbrowser.open("https://www.youtube.com/channel/UCC-KnP22oPHfTt_vM4X5_6g")
+            
+            elif what1 == "d":
+                os.system('cls' if os.name=='nt' else 'clear')
+                time.sleep(1)
+                print("Abriendo...  \n")
+                time.sleep(1)
+                os.system('cls' if os.name=='nt' else 'clear')
+                webbrowser.open("https://www.youtube.com/channel/UCCKj17GGbXvxpnECIJPUrfg/featured")
             
             elif what1 == "x":
                 os.system('cls' if os.name=='nt' else 'clear')
